6ed3f683b03d9df7f36ee4be2da687a2c0f2a10a
Luann244 <weversonluan17@gmail.com>
2021-11-27 22:24:49 -0300

    setup component-Header

--- portfolio/src/components/Header/styles.ts
+++ portfolio/src/components/Header/styles.ts
@@ -1,9 +1,4 @@
 import styled from "styled-components";
 
-export const Container = styled.nav`
-  width: 100%;
-  border: ${(props) => `1px solid ${props.theme.colors.onyx}`};
-  background-color: ${(props) => props.theme.colors.lightBlue};
-  font-family: ${(props) => props.theme.fonts[0]};
-`;
+export const Container = styled.nav``;
 export const Title = styled.p``;
