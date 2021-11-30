4ffc04330c5a3b15d2c1c798553d3b2204766da2
Luann244 <weversonluan17@gmail.com>
2021-11-30 20:58:15 -0300

    finalizado: Component Section-About

--- portfolio/src/components/SectionAbout/index.tsx
+++ portfolio/src/components/SectionAbout/index.tsx
@@ -1,8 +1,26 @@
-import { Container, Title } from "./styles";
+import { Container, Title, TextAbout } from "./styles";
 export function SectionAbout() {
   return (
     <Container>
-      <Title>Hellowrd ProgramadorWL !</Title>
+      <Title> Sobre mim </Title>
+
+      <TextAbout>
+        Olá, tudo bem? :) Sou profissional e especialista em desenvolvimento de
+        aplicativos (mobile) e Web. Apesar de novo no mercado de trabalho de
+        programação, obtive bastante experiência em desenvolvimento de sistemas.
+        Durante os vários anos que venho constantemente estudando e
+        desenvolvendo aplicações (mobiles) e aplicações web. Minhas principais
+        habilidades são HTML5, CSS3, JS, NODE, TYPESCRIPT (tenho algumas
+        certificações), e trabalhando com algumas bibliotecas como
+        [React.js](http://React.js), [React.next](http://React.next) e
+        frameworks como Express ( [Node.js](http://Node.js) ). Também tenho
+        experiência em desenvolvimento FullStack e muito mais! Todos meus
+        projetos são focados em design de interação, levando em consideração
+        sempre as boas práticas de programação, seguindo sempre os padrões de
+        desenvolvimento mobile e web, tais como as recomendações do Google e da
+        comunidade, para atingir os melhores índices de desempenho e conversão!
+        Segue meu Portifólio.
+      </TextAbout>
     </Container>
   );
 }
--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../components/Header";
 import { HeaderSection } from "../components/SectionHeader";
+import { SectionAbout } from "../components/SectionAbout";
 export default function App() {
   return (
     <>
       <Header />
       <HeaderSection />
+      <SectionAbout />
     </>
   );
 }
