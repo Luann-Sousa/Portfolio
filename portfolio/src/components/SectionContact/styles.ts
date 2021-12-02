7bfac3ec97f37970660d427f630391c01be6bca9
Luann244 <weversonluan17@gmail.com>
2021-12-02 19:35:03 -0300

    finaly: component SectionContac foi finalizado

--- portfolio/src/components/SectionContact/styles.ts
+++ portfolio/src/components/SectionContact/styles.ts
@@ -1,4 +1,56 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
-export const Title = styled.p``;
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  margin-bottom: 100px;
+`;
+export const ContainerIcon = styled.div`
+  width: 62px;
+  height: 62px;
+
+  border-radius: 30px;
+
+  background-color: ${({ theme }) => theme.colors.gray_150};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const ImagageUser = styled.img`
+  width: 62px;
+  height: 62px;
+
+  border-radius: 30px;
+`;
+export const WrappeerContact = styled.div`
+  width: 150px;
+  height: 130px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Title = styled.p`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 23px;
+
+  color: #f9f9f9;
+`;
+export const SubTitle = styled.p`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 21px;
+  /* identical to box height */
+
+  color: #828282;
+`;
