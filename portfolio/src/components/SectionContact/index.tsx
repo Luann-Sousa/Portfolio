5cd2c31062426be4e4d2f76d2731b136a3ef44d2
Luann244 <weversonluan17@gmail.com>
2021-12-02 18:47:37 -0300

    create: component SectionContac foi criado

---
+++ portfolio/src/components/SectionContact/index.tsx
@@ -0,0 +1,8 @@
+import { Container, Title } from "./styles";
+export const SectionContact = () => {
+  return (
+    <Container>
+      <Title>Hellow ProgramadorWL !</Title>
+    </Container>
+  );
+};
