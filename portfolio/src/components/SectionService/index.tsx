cc58680c2f358d6f248801ce8ef7ceaba6acdc89
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-01-07 00:10:00 -0300

    create/component: foi criado o componente SectionService

---
+++ portfolio/src/components/SectionService/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export const SectionService = () => {
+  return (
+    <Container>
+      <Title>ProgramadorWL</Title>
+    </Container>
+  );
+};
